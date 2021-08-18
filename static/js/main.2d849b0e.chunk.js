efc71c4282edbafffadda6cf1a78f9f73300e097
Code Sandbox <hello@codesandbox.io>
2021-08-18 10:35:09 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.2d849b0e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmovie-recommendation"]=this["webpackJsonpmovie-recommendation"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),o=n(3),i=n.n(o),r=(n(8),n(0));function s(){return Object(r.jsxs)("div",{className:"App",children:[Object(r.jsx)("h1",{children:"Hello CodeSandbox"}),Object(r.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var d=document.getElementById("root");i.a.render(Object(r.jsx)(c.StrictMode,{children:Object(r.jsx)(s,{})}),d)},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.2d849b0e.chunk.js.map
